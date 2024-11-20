e4453ad632fa217171ef23af23516919da54bbb7
Allihkak <chertenok231@gmail.com>
2024-11-20 07:49:03 +0500

    Add files via upload

---
+++ Peremennye.py
@@ -0,0 +1,13 @@
+home_work = 12
+number_of_hours = 1.5
+name = 'Pyton'
+print(name)
+name ='Домашних заданий'
+print(name),print(home_work)
+name = 'Затрачено часов'
+print(name),print(number_of_hours)
+name = 'Время на 1 задание'
+print(name)
+a=(number_of_hours)/(home_work)
+print(a)
+
